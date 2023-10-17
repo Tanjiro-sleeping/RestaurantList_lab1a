a825820f8a624f36a919e8b0bcd9dbdd537a5c9c
Westq2005 <tzesheng.cod@gmail.com>
2023-10-17 16:50:17 +0800

    Initial commit

---
+++ app/src/main/java/com/sp/restaurantlist/RestuarantList.java
@@ -0,0 +1,14 @@
+package com.sp.restaurantlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestuarantList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
